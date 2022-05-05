c3d169f3c72c23c746fda8f5efe921d3b19a5d51
OrdinarilySam <OrdinarilySam@gmail.com>
2022-05-05 18:13:33 -0500

    slightly changed some of class functions

--- pwgen.py
+++ pwgen.py
@@ -1,30 +1,35 @@
 import random
 
 class Password:
+
+    # variables to check and store the password
     chars = {"lower":"abcdefghijklmnopqrstuvwxyz",
             "upper":"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
             "numbers":"0123456789",
             "symbols":" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"}
     password = ""
-    def __init__(self, lower=True, upper=True, nums=True, sym=True):
+    
+    # initializes objects with parameters
+    def __init__(self, length=12, lower=True, upper=True, nums=True, sym=True):
+        self.length = length
         self.lower = lower
         self.upper = upper
         self.nums = nums
         self.sym = sym
 
+    # sets the value for when the object is called on 
     def __str__(self):
         return f"Your password is {self.password}"
 
     @classmethod
     def generate(cls):
-        ...
+        valid = False
+        while not valid:
+            ... #
 
     def check(self):
         ...
     
-    def create(self):
-        ...
-
 
 def main():
     password = Password().generate()
