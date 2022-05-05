bd64c727036fe4798e0a919d56835a9dc5de50dc
OrdinarilySam <OrdinarilySam@gmail.com>
2022-05-05 17:33:23 -0500

    switched to an object oriented approach

--- pwgen.py
+++ pwgen.py
@@ -1,26 +1,17 @@
-from random import *
+import random
 
-#global variables
+class Password:
+    def __str__(self):
+        ...
+
+    def generate(self):
+        ...
+
+    def check(self):
+        ...
+    
+
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
-def main():
-    print(gen_passwd(12))
-
-def gen_passwd(length):
-    valid = False
-    string = lower  + upper + numbers + symbols
-    while not valid:
-        pwd = ""
-        for i in range(length):
-            pwd += pwd.join(string[randint(0,len(string)-1)])
-        valid = check_passwd(pwd)
-    return pwd
-
-def check_passwd(pswd):
-    pass
-
-if __name__ == "__main__":
-    main()
