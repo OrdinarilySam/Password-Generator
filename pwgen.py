ccb61fffb30fe57182bcc89457ea68e00f406e0b
OrdinarilySam <OrdinarilySam@gmail.com>
2022-08-02 17:22:46 -0500

    added checks

--- pwgen.py
+++ pwgen.py
@@ -3,19 +3,15 @@ import random
 class Password:
 
     # variables to check and store the password
-    chars = {"lower":"abcdefghijklmnopqrstuvwxyz",
-            "upper":"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            "numbers":"0123456789",
-            "symbols":" !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"}
+    chars = ["abcdefghijklmnopqrstuvwxyz",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "0123456789",
+            " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
     password = ""
     
     # initializes objects with parameters
-    def __init__(self, length=12, lower=True, upper=True, nums=True, sym=True):
+    def __init__(self, length=12):
         self.length = length
-        self.lower = lower
-        self.upper = upper
-        self.nums = nums
-        self.sym = sym
 
     # sets the value for when the object is called on 
     def __str__(self):
